7376fd75f1e91441c54ba4a8c4903fa3e9befd5f
Mihai Giurgeanu <mihai.giurgeanu@gmail.com>
2015-02-20 01:30:52 +0200

    Prpared production version.

--- httpprobe/src/httpprobe/probes.clj
+++ httpprobe/src/httpprobe/probes.clj
@@ -13,30 +13,25 @@
   [host path]
   (str "http://" host path))
 
-(defn- create-request [{:keys [host path]}]
+(defn- create-request [{:keys [host path] :as req-pair}]
   (let [permissions-channel *permissions-channel*
         responses-channel *responses-channel*
         pending-requests *pending-requests*
         this-request-promise (promise)]
-    (println "Sending request to " host path (System/nanoTime))
     (send pending-requests conj this-request-promise)
     (try
       (http/get (make-url host path) *http-options*
                 (fn [response]
-                  (println "Received response for " host path)
                   (try
-                    (let [receiveddate (System/nanoTime)]
-                      (println "Sending permission" receiveddate)
-                      (put! permissions-channel receiveddate)
-                      (put! responses-channel response)
-                      (deliver this-request-promise receiveddate))
+                    (put! permissions-channel req-pair)
+                    (put! responses-channel response)
+                    (deliver this-request-promise 1)
                     (catch Exception e
                       (println "Exception sending permission and response" (.getMessage e))))))
       (catch Exception e
         (println "Exception creating a request for" host path (.getMessage e))
-        (let [receiveddate (System/nanoTime)]
-          (put! permissions-channel receiveddate)
-          (deliver this-request-promise receiveddate))))))
+          (put! permissions-channel req-pair)
+          (deliver this-request-promise 1)))))
 
 (defn- display-response
   [{:keys [opts body status headers error]}]
@@ -52,40 +47,34 @@
   hosts in the list. Retrieves info if the path si valid
   link and, if yes, gets the title of the page"
   [hosts paths batch-size]
-  (println "Sending probes:" (System/nanoTime))
   (binding [*permissions-channel* (chan batch-size)
             *responses-channel* (chan batch-size)
             *pending-requests* (agent [])]
     (let [requests (for [h hosts p paths] {:host h :path p})
           first-batch (take batch-size requests)
           rest-batch (drop batch-size requests)
-          requests-finished (agent false)]
+          requests-finished (agent false)
+          start-time (System/nanoTime)]
       (doseq [r first-batch]
         (create-request r))
       (go-loop [unprocessed-requests rest-batch process-list true]
-               (println "Waiting permission to send request")
                (let [permission (<! *permissions-channel*)]
                  (if process-list
                    (if (not-empty unprocessed-requests)
                      (let [[req & reqs] unprocessed-requests]
-                       (println "Received persmission"
-                                (:host req)
-                                (:path req)
-                                permission)
                        (create-request req)
                        (recur reqs true))
                      (do
-                       (println "All requests enqued" (System/nanoTime))
                        (send requests-finished (fn [state] true))
                        (recur nil false)))
                    (when permission (recur nil false)))))
 
       (loop []
         (when-let [response (<!! *responses-channel*)]
-          ;(display-response response)
+          (display-response response)
           (when (or (not @requests-finished) (not-every? realized? @*pending-requests*))
             (recur))))
-      (println "Done!" (System/nanoTime))
+      (println "Done!" (/(- (System/nanoTime) start-time) 1000000) "ms")
       (close! *permissions-channel*)
       (close! *responses-channel*)
       (shutdown-agents))))
