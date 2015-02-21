fb9c4c6660f9f4c821e8ced99a243bb7a1faf717
Mihai Giurgeanu <mihai.giurgeanu@gmail.com>
2015-02-21 16:52:09 +0200

    New release.

--- httpprobe/project.clj
+++ httpprobe/project.clj
@@ -1,4 +1,4 @@
-(defproject httpprobe "0.1.2-SNAPSHOT"
+(defproject httpprobe "0.1.2"
   :description "Sends many http GET request"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
